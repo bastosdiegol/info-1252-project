162b45689ffcf5483f6744af1a589dcadc5777bb
Diego Bastos <bastosdiegol@gmail.com>
2023-12-08 05:13:36 -0500

    Form Section and Resume Placeholder

--- scripts/events.js
+++ scripts/events.js
@@ -1,4 +1,7 @@
-// Event that Hides and Shows the Menu
+// Sets the menu initial state as hidden
+document.getElementById("menuList").style.display = "none";
+
+// Adds Event that Hides and Shows the Menu
 addEventListener("load", (event) => {
   // Grabs the Button
   const button = document.getElementById("toggleMenuButton");
@@ -7,11 +10,12 @@ addEventListener("load", (event) => {
   // Adds the Toggle Menu Event
   button.addEventListener("click", (event) => {
     // Toggle the display property of the menu
-    // menu.style.display = menu.style.display === "none" ? "flex" : "none";
+    menu.style.display = menu.style.display === "none" ? "flex" : "none";
     // Toggle the Fading
     menu.classList.toggle("fade-in");
     menu.classList.toggle("fade-out");
   });
 });
 
+// Sets the current year in the footer
 document.getElementById("year").textContent = new Date().getFullYear();
