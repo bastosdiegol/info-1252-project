81ae90e4bdbba98fb740f2f82a94640cfd78bfd7
Diego Bastos <bastosdiegol@gmail.com>
2023-12-08 20:30:53 -0500

    Menu Visibility Fix for dynamically width change

--- scripts/events.js
+++ scripts/events.js
@@ -1,12 +1,41 @@
-// Sets the menu initial state as hidden
-document.getElementById("menuList").style.display = "none";
+/**
+ * Function that changes the menu visibility in case of width change.
+ */
+function updateMenuVisibility() {
+  // Grabs the Nav
+  const menu = document.getElementById("menuList");
+  // Gets the window width
+  const screenWidth = window.innerWidth;
+  // Validates the min-width for Mobile
+  if (screenWidth < 768) {
+    // Mobile Width Hide the Menu
+    menu.style.display = "none";
+    menu.classList.remove("fade-in");
+    menu.classList.add("fade-out");
+  } else {
+    // Tablet+ Witdh Sets the Menu as active
+    menu.style.display = "flex";
+    menu.classList.remove("fade-out");
+    menu.classList.add("fade-in");
+  }
+}
+
+// Initial call for first load
+addEventListener("load", (event) => {
+  updateMenuVisibility();
+});
+
+// Update menu visibility on window resize
+addEventListener("resize", (event) => {
+  updateMenuVisibility();
+});
 
 // Adds Event that Hides and Shows the Menu
 addEventListener("load", (event) => {
-  // Grabs the Button
-  const button = document.getElementById("toggleMenuButton");
   // Grabs the Nav
   const menu = document.getElementById("menuList");
+  // Grabs the Button
+  const button = document.getElementById("toggleMenuButton");
   // Adds the Toggle Menu Event
   button.addEventListener("click", (event) => {
     // Toggle the display property of the menu
@@ -15,16 +44,6 @@ addEventListener("load", (event) => {
     menu.classList.toggle("fade-in");
     menu.classList.toggle("fade-out");
   });
-  // Gets the window width
-  const screenWidth = window.innerWidth;
-  console.log(screenWidth);
-  // Validates the min-width
-  if (screenWidth >= 768) {
-    // Sets the Menu as active
-    menu.style.display = "flex";
-    menu.classList.remove("fade-out");
-    menu.classList.add("fade-in");
-  }
 });
 
 // Sets the current year in the footer
